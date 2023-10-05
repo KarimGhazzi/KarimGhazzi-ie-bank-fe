0710e52ea506f975a0a33039fc273269f8edfc3a
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2023-10-05 16:42:31 +0200

    xxxxx

---
+++ src/components/homepage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <header>
+        <h1>Welcome to MyBank</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/accounts">Accounts</router-link></li>
+          <li><router-link to="/skull">Skull</router-link></li>
+        </ul>
+      </nav>
+      <router-view></router-view>
+      <footer>
+        <p>&copy; 2023 MyBank</p>
+      </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BankHomepage',
+  }
+  </script>
+  
+  <style>
+  /* Add your CSS styles here */
+  </style>
+  
